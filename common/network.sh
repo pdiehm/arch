d50183be80556cc9b897feb1da792e8749b7ecc1
Pascal Diehm <pdiehm8@gmail.com>
2026-04-14 17:51:05 +0200

    feat(network): configure systemd-resolved and link resolv.conf

--- common/network.sh
+++ common/network.sh
@@ -1,2 +1,23 @@
+write /etc/systemd/resolved.conf << EOF
+[Resolve]
+DNS=1.1.1.1#cloudflare-dns.com 1.0.0.1#cloudflare-dns.com 2606:4700:4700::1111#cloudflare-dns.com 2606:4700:4700::1001#cloudflare-dns.com 8.8.8.8#dns.google 8.8.4.4#dns.google 2001:4860:4860::8888#dns.google 2001:4860:4860::8844#dns.google
+Domains=~.
+LLMNR=no
+MulticastDNS=no
+DNSOverTLS=opportunistic
+EOF
+
+write /etc/systemd/system/resolvconf.service << EOF
+[Unit]
+Description=Link systemd-resolved stub-resolv.conf to /etc/resolv.conf
+
+[Install]
+WantedBy=systemd-resolved.service
+
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+EOF
+
 write /etc/hostname "$HOST_NAME"
-run systemctl enable systemd-resolved.service
+run systemctl enable systemd-resolved.service resolvconf.service
