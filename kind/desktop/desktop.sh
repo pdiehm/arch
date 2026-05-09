67fb23545bf2becb7e7145e54221d37cd4e94b89
Pascal Diehm <pdiehm8@gmail.com>
2026-05-09 10:21:51 +0200

    refactor(hypr): link wallpaper to home directory

--- kind/desktop/desktop.sh
+++ kind/desktop/desktop.sh
@@ -7,6 +7,7 @@ symlink -u res/fontconfig.xml .config/fontconfig/fonts.conf
 
 package hyprland hyprpaper hyprlock hypridle hyprshot hyprpicker xdg-desktop-portal-hyprland
 symlink -u res/hypr .config/hypr
+symlink -u res/wallpaper.jpg .local/share/wallpaper.jpg
 
 package kitty waybar rofi mako
 symlink -u res/waybar .config/waybar
