d1a0f79f278cb495955942c9e61ccf2d5ab103f2
Pascal Diehm <pdiehm8@gmail.com>
2026-05-09 21:17:25 +0200

    feat(ssh): add ssh-agent

--- kind/desktop/ssh.sh
+++ kind/desktop/ssh.sh
@@ -7,3 +7,7 @@ copy -nsu ssh/github .ssh/github
 package sshfs
 symlink -u res/systemd/user/home-pascal-Shared.mount .config/systemd/user/home-pascal-Shared.mount
 symlink -u /home/pascal/.config/systemd/user/home-pascal-Shared.mount .config/systemd/user/default.target.wants/home-pascal-Shared.mount
+
+write -au .config/dropin/env "SSH_AUTH_SOCK=\"\$XDG_RUNTIME_DIR/ssh-agent.socket\""
+symlink -u res/systemd/user/ssh-agent.service .config/systemd/user/ssh-agent.service.d/override.conf
+symlink -u /usr/lib/systemd/user/ssh-agent.service .config/systemd/user/default.target.wants/ssh-agent.service
