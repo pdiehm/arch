2194b8a9290adc25fe26dca1744fe04f55ee2d7b
Pascal Diehm <pdiehm8@gmail.com>
2026-05-09 11:19:58 +0200

    feat: add SSH keys for bowser and goomba

--- common/network.sh
+++ common/network.sh
@@ -3,3 +3,9 @@ write /etc/hostname "$HOST_NAME"
 copy res/systemd/resolved.conf /etc/systemd/resolved.conf
 copy res/systemd/resolvconf.service /etc/systemd/system/resolvconf.service
 run systemctl enable systemd-resolved.service resolvconf.service
+
+write -a /etc/hosts << EOF
+192.168.1.88         bowser
+91.99.52.233         goomba
+2a01:4f8:c0c:988b::1 goomba
+EOF
--- kind/desktop/main.sh
+++ kind/desktop/main.sh
@@ -5,6 +5,8 @@ import ./yubikey
 
 symlink -u res/ssh/ssh_config .ssh/config
 copy -nsu ssh/arch .ssh/arch
+copy -nsu ssh/bowser .ssh/bowser
+copy -nsu ssh/goomba .ssh/goomba
 copy -nsu ssh/github .ssh/github
 
 package git-delta
