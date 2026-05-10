c946b6fa2201b3b8affc3bafeb95547d8b1d930b
Pascal Diehm <pdiehm8@gmail.com>
2026-05-10 21:38:28 +0200

    feat(network): add nftables

--- common/network.sh
+++ common/network.sh
@@ -1,6 +1,9 @@
 write /etc/hostname "$HOST_NAME"
 copy res/hosts /etc/hosts
 
+copy res/nftables.conf /etc/nftables.conf
+run systemctl enable nftables.service
+
 copy res/systemd/resolved.conf /etc/systemd/resolved.conf
 copy res/systemd/system/resolvconf.service /etc/systemd/system/resolvconf.service
 run systemctl enable systemd-resolved.service resolvconf.service
