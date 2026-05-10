146065c0a993a982d5d889bf9f80818887405594
Pascal Diehm <pdiehm8@gmail.com>
2026-05-10 10:56:42 +0200

    feat(desktop): setup firefox

--- kind/desktop/main.sh
+++ kind/desktop/main.sh
@@ -7,5 +7,9 @@ import ./yubikey
 package git-delta
 symlink -u res/git.conf .config/git/config
 
+package firefox
+persist -u .config/mozilla/firefox
+symlink res/firefox.json /etc/firefox/policies/policies.json
+
 package pipewire pipewire-alsa wireplumber alsa-utils wiremix
 write -a /etc/fstab "tmpfs /home/pascal/Temp tmpfs defaults 0 0"
