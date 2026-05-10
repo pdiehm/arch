b746f9f5aec47f0783afcfce507c8bf5e96b09d7
Pascal Diehm <pdiehm8@gmail.com>
2026-05-10 21:49:15 +0200

    feat(nvim): add oil keymaps

--- res/nvim/lua/user/plugins/oil.lua
+++ res/nvim/lua/user/plugins/oil.lua
@@ -7,8 +7,18 @@ require("oil").setup({
     ["<Return>"] = "actions.select",
     ["<BS>"] = "actions.parent",
     ["q"] = "actions.close",
+    ["r"] = "actions.refresh",
     ["_"] = "actions.open_cwd",
     ["~"] = "actions.cd",
+    ["1"] = { "1j<CR>", remap = true },
+    ["2"] = { "2j<CR>", remap = true },
+    ["3"] = { "3j<CR>", remap = true },
+    ["4"] = { "4j<CR>", remap = true },
+    ["5"] = { "5j<CR>", remap = true },
+    ["6"] = { "6j<CR>", remap = true },
+    ["7"] = { "7j<CR>", remap = true },
+    ["8"] = { "8j<CR>", remap = true },
+    ["9"] = { "9j<CR>", remap = true },
   },
 })
 
