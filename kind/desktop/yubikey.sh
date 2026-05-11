9029d30aace67d3b24694ddcdcbc44d25c3bc8da
Pascal Diehm <pdiehm8@gmail.com>
2026-05-11 14:44:21 +0200

    refactor(yubikey): move scripts to libexec

--- kind/desktop/yubikey.sh
+++ kind/desktop/yubikey.sh
@@ -2,8 +2,8 @@ package yubikey-manager pam-u2f evtest
 write -a /etc/gnupg/scdaemon.conf disable-ccid
 run systemctl enable pcscd.service
 
-copy res/bin/yubikey-lock /usr/local/bin/yubikey-lock
-copy res/bin/yubikey-unlock /usr/local/bin/yubikey-unlock
+copy -x res/scripts/yubikey-lock.sh /usr/local/libexec/yubikey-lock
+copy -x res/scripts/yubikey-unlock.sh /usr/local/libexec/yubikey-unlock
 copy res/udev/yubikey.rules /etc/udev/rules.d/yubikey.rules
 
 copy -su u2f_keys .config/Yubico/u2f_keys
