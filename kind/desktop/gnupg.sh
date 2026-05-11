6c20f446ef128fcd4cac13bffb9a6fb574fac762
Pascal Diehm <pdiehm8@gmail.com>
2026-05-11 14:55:05 +0200

    fix(gnupg): import key to correct location

--- kind/desktop/gnupg.sh
+++ kind/desktop/gnupg.sh
@@ -2,6 +2,7 @@ persist -u -m 700 .local/share/gnupg
 write -a /etc/gnupg/gpg-agent.conf "pinentry-program /usr/bin/pinentry-tty"
 
 script -u << EOF
+export GNUPGHOME="/home/pascal/.local/share/gnupg"
 gpg --import "$(use res/key.gpg)"
 gpg --quick-set-ownertrust 32104A99C1849AF79B2C92FCE85EB0566C779A2F ultimate
 EOF
