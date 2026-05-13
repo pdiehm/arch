e8bcb5e0c5b19c8b556f84d642d39c902504d59a
Pascal Diehm <pdiehm8@gmail.com>
2026-05-13 22:12:05 +0200

    feat(hypr): remove CJK font

--- kind/desktop/hypr.sh
+++ kind/desktop/hypr.sh
@@ -2,7 +2,7 @@ package greetd greetd-tuigreet
 copy res/greetd.toml /etc/greetd/config.toml
 run systemctl enable greetd.service
 
-package noto-fonts noto-fonts-extra noto-fonts-emoji noto-fonts-cjk ttf-firacode-nerd
+package noto-fonts noto-fonts-extra noto-fonts-emoji ttf-firacode-nerd
 symlink -u res/fontconfig.xml .config/fontconfig/fonts.conf
 
 package hyprland hyprpaper hyprlock hypridle hyprshot hyprpicker xdg-desktop-portal-hyprland
