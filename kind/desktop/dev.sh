a309ce15aadb68339807c2610bf38e50206a53de
Pascal Diehm <pdiehm8@gmail.com>
2026-05-16 22:13:23 +0200

    refactor(desktop): move git and repo script to dev module

--- kind/desktop/dev.sh
+++ kind/desktop/dev.sh
@@ -4,3 +4,10 @@ write -au .config/dropin/env.sh << EOF
 CMAKE_GENERATOR="Ninja"
 CMAKE_EXPORT_COMPILE_COMMANDS="ON"
 EOF
+
+package git git-delta
+symlink -u res/git.conf .config/git/config
+
+persist -u Repos
+symlink -u res/bin/repo.sh .local/bin/repo
+timer -nu repo-fetch hourly "%h/.local/bin/repo" fetch
--- kind/desktop/main.sh
+++ kind/desktop/main.sh
@@ -9,13 +9,6 @@ import ./yubikey
 copy res/systemd/logind.conf /etc/systemd/logind.conf
 write /etc/sysctl.d/sysrq.conf "kernel.sysrq = 1"
 
-package git git-delta
-symlink -u res/git.conf .config/git/config
-
-persist -u /home/pascal/Repos
-symlink -u res/bin/repo.sh .local/bin/repo
-timer -nu repo-fetch hourly "%h/.local/bin/repo" fetch
-
 package firefox
 persist -u .config/mozilla/firefox
 symlink res/firefox.json /etc/firefox/policies/policies.json
