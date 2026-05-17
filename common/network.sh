db5bb85cf3f36992b4a0ab407e8188fe9010c7c9
Pascal Diehm <pdiehm8@gmail.com>
2026-05-17 12:46:57 +0200

    feat: remove dynhostmgr

--- common/network.sh
+++ common/network.sh
@@ -1,18 +1,13 @@
-package dynhostmgr
-systemd -e dynhostmgr.service
 write /etc/hostname "$HOST_NAME"
 
 write /etc/hosts << EOF
 127.0.0.1            localhost
 ::1                  localhost
+192.168.1.88         bowser
 192.168.1.89         homeassistant
+192.168.1.90         arch
 91.99.52.233         goomba
 2a01:4f8:c0c:988b::1 goomba
-EOF
-
-write /etc/dynhosts << EOF
-arch   192.168.1.90
-bowser 192.168.1.88
 EOF
 
 copy res/nftables.conf /etc/nftables.conf
