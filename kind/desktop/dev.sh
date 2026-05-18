b9db7438a54586b2d141504fe2c4ce5009ae42d3
Pascal Diehm <pdiehm8@gmail.com>
2026-05-18 10:23:17 +0200

    feat(desktop): add nix

--- kind/desktop/dev.sh
+++ kind/desktop/dev.sh
@@ -5,6 +5,12 @@ package make cmake ninja gcc gdb rust python python-pip nodejs npm jdk-openjdk s
 package paru base-devel devtools nvchecker
 conf -e /etc/paru.conf BottomUp CleanAfter RemoveMake SudoLoop
 
+package aur/nix
+persist /nix
+copy res/nix.conf /etc/nix/nix.conf
+systemd -e nix-daemon.service
+timer nix-gc monthly /usr/bin/nix-collect-garbage --delete-old
+
 write -au .config/dropin/env.sh << EOF
 CMAKE_GENERATOR="Ninja"
 CMAKE_EXPORT_COMPILE_COMMANDS="ON"
