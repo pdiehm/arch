8e3ff6195921a09a47cee4dad0b813c2480d3392
Pascal Diehm <pdiehm8@gmail.com>
2026-05-18 23:51:04 +0200

    fix(nix): bind mount /nix and set NIX_PATH environment variable

--- kind/desktop/dev.sh
+++ kind/desktop/dev.sh
@@ -2,19 +2,22 @@ package make cmake ninja gdb strace gcc rust python python-pip nodejs npm jdk-op
   texlive-basic texlive-latex texlive-latexrecommended texlive-fontsrecommended texlive-langgerman \
   texlive-binextra perl-file-homedir perl-yaml-tiny
 
-package paru base-devel devtools nvchecker
-conf -e /etc/paru.conf BottomUp CleanAfter RemoveMake SudoLoop
-
-package aur/nix
-persist /nix
-copy res/nix.conf /etc/nix/nix.conf
-systemd -e nix-daemon.service
-timer nix-gc monthly /usr/bin/nix-collect-garbage --delete-old
-
 write -au .config/dropin/env.sh << EOF
 CMAKE_GENERATOR="Ninja"
 CMAKE_EXPORT_COMPILE_COMMANDS="ON"
 EOF
+
+package nix
+copy res/nix.conf /etc/nix/nix.conf
+systemd -e nix-daemon.service
+timer nix-gc monthly /usr/bin/nix-collect-garbage --delete-old
+
+run mkdir /perm/nix
+write -a /etc/fstab "/perm/nix /nix none bind 0 0"
+write -au .config/env.sh "NIX_PATH=nixpkgs=flake:nixpkgs"
+
+package paru base-devel devtools nvchecker
+conf -e /etc/paru.conf BottomUp CleanAfter RemoveMake SudoLoop
 
 package git git-delta
 symlink -u res/git.conf .config/git/config
