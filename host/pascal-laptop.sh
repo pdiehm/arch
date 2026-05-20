f358c1cca1bea44fabc1667002205dc705bf1dc0
Pascal Diehm <pdiehm8@gmail.com>
2026-05-20 12:34:55 +0200

    feat(pascal-laptop): add tlp

--- host/pascal-laptop.sh
+++ host/pascal-laptop.sh
@@ -1,5 +1,3 @@
-dropin hyprland.lua 'hl.monitor({ output = "eDP-1", mode = "preferred", position = "auto", scale = 1.33 })'
-
 env NAME home-wifi
 env PRIORITY 50
 env SSID "$(secret network/home-wifi/ssid)"
@@ -7,3 +5,9 @@ env PSK "$(secret network/home-wifi/psk)"
 env ADDRESS 192.168.1.91/16
 env GATEWAY 192.168.1.1
 copy -em 400 res/NetworkManager/wifi-static.nmconnection /etc/NetworkManager/system-connections/home-wifi.nmconnection
+
+package tlp
+systemd -m systemd-rfkill.service systemd-rfkill.socket
+systemd -e tlp.service
+
+dropin hyprland.lua 'hl.monitor({ output = "eDP-1", mode = "preferred", position = "auto", scale = 1.33 })'
