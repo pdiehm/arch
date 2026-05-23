2105e0366688412c29568f4c281ae9e79782c6f5
Pascal Diehm <pdiehm8@gmail.com>
2026-05-23 09:36:59 +0200

    feat(desktop): specify default applications

--- kind/desktop/applications.sh
+++ kind/desktop/applications.sh
@@ -1,7 +1,10 @@
-package dolphin gwenview archlinux-xdg-menu ffmpegthumbs kdegraphics-thumbnailers
+package xdg-utils archlinux-xdg-menu
+symlink -u res/mimeapps.list .config/mimeapps.list
+copy -u res/user-places.xml .local/share/user-places.xbel
+
+package dolphin gwenview ffmpegthumbs kdegraphics-thumbnailers
 copy -u res/dolphin.toml .config/dolphinrc
 copy -u res/gwenview.toml .config/gwenviewrc
-copy -u res/user-places.xml .local/share/user-places.xbel
 
 package firefox
 persist -u .config/mozilla/firefox
