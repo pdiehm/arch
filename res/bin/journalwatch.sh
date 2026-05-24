7a52bcb30167b9966d9ff33d766dd63fd4d39284
Pascal Diehm <pdiehm8@gmail.com>
2026-05-24 11:58:58 +0200

    fix(journalwatch): ignore sudo from root

--- res/bin/journalwatch.sh
+++ res/bin/journalwatch.sh
@@ -3,7 +3,7 @@
 SCRIPT="s/^\w+ \w+ \S+ \S+ ([^:[]+)[^:]*: (.+)$/\1:::\2/;"
 SCRIPT+="s/^systemd:::Startup finished in .+ = (.+)\.$/[systemd] Booted in \1/p;"
 SCRIPT+="s/^systemd:::(\S+): Failed with result '(.+)'\.$/[systemd] \1 failed: \2/p;"
-SCRIPT+="s/^sudo:::\s+(\S+) : .* USER=(\S+) .* COMMAND=(.+)$/[sudo] \1 as \2: \3/p;"
+SCRIPT+="/^sudo:::\s*root : .*$/! s/^sudo:::\s+(\S+) : .* USER=(\S+) .* COMMAND=(.+)$/[sudo] \1 as \2: \3/p;"
 SCRIPT+="s/^sshd-session:::(Accepted|Failed) password for (\S+) from (\S+) port \w+ ssh2$/[ssh] \1 password for \2 from \3/p;"
 SCRIPT+="s/^sshd-session:::(Accepted|Failed) publickey for (\S+) from (\S+) port \w+ ssh2: .*$/[ssh] \1 publickey for \2 from \3/p;"
 SCRIPT+="s/^sshd-session:::Invalid user (\S+) from (\S+) port \w+$/[ssh] Invalid user \1 from \2/p;"
