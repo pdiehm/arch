6fd53cddda84405f5c7b119a803b536180c81dcd
Pascal Diehm <pdiehm8@gmail.com>
2026-05-24 07:31:45 +0200

    refactor: rename network secret group to net

--- host/pascal-laptop.sh
+++ host/pascal-laptop.sh
@@ -1,7 +1,7 @@
 env NAME home-wifi
 env PRIORITY 50
-env SSID "$(secret network/home-wifi/ssid)"
-env PSK "$(secret network/home-wifi/psk)"
+env SSID "$(secret net/home-wifi/ssid)"
+env PSK "$(secret net/home-wifi/psk)"
 env ADDRESS 192.168.1.91/16
 env GATEWAY 192.168.1.1
 copy -em 400 res/NetworkManager/wifi-static.nmconnection /etc/NetworkManager/system-connections/home-wifi.nmconnection
--- kind/desktop/network.sh
+++ kind/desktop/network.sh
@@ -3,16 +3,16 @@ systemd -e NetworkManager.service
 
 env NAME hotspot
 env PRIORITY 25
-env SSID "$(secret network/hotspot/ssid)"
-env PSK "$(secret network/hotspot/psk)"
+env SSID "$(secret net/hotspot/ssid)"
+env PSK "$(secret net/hotspot/psk)"
 copy -em 400 res/NetworkManager/wifi-dhcp.nmconnection /etc/NetworkManager/system-connections/hotspot.nmconnection
 
 env NAME eduroam
 env PRIORITY 50
 env SSID eduroam
-env IDENTITY "$(secret network/eduroam/identity)"
-env PASSWORD "$(secret network/eduroam/password)"
-copy -s network/eduroam/private-key /etc/NetworkManager/system-connections/eduroam/private-key
-copy -s network/eduroam/client-cert /etc/NetworkManager/system-connections/eduroam/client-cert
-copy -s network/eduroam/ca-cert /etc/NetworkManager/system-connections/eduroam/ca-cert
+env IDENTITY "$(secret net/eduroam/identity)"
+env PASSWORD "$(secret net/eduroam/password)"
+copy -s net/eduroam/private-key /etc/NetworkManager/system-connections/eduroam/private-key
+copy -s net/eduroam/client-cert /etc/NetworkManager/system-connections/eduroam/client-cert
+copy -s net/eduroam/ca-cert /etc/NetworkManager/system-connections/eduroam/ca-cert
 copy -em 400 res/NetworkManager/wifi-eap.nmconnection /etc/NetworkManager/system-connections/eduroam.nmconnection
