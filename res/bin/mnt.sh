a6d1c17883df31c2e74ca47d5c11ce1cf98b107d
Pascal Diehm <pdiehm8@gmail.com>
2026-05-25 21:32:48 +0200

    refactor(mnt): use FD 3

--- res/bin/mnt.sh
+++ res/bin/mnt.sh
@@ -12,18 +12,18 @@ if [[ -z $DEV ]]; then
   exit 1
 elif [[ $DEV == tmpfs ]]; then
   sudo mount -t tmpfs tmpfs "$TMP"
-  exec 5> >(sudo bash -c "$UNMOUNT")
+  exec 3> >(sudo bash -c "$UNMOUNT")
 elif [[ $DEV == android ]]; then
   aft-mtp-mount "$TMP"
-  exec 5> >(bash -c "$UNMOUNT")
+  exec 3> >(bash -c "$UNMOUNT")
 elif [[ $DEV =~ ssh://([^:]+)(:(.*))? ]]; then
   sshfs -o idmap=user -o transform_symlinks -o follow_symlinks "${BASH_REMATCH[1]}:${BASH_REMATCH[3]}" "$TMP"
-  exec 5> >(bash -c "$UNMOUNT")
+  exec 3> >(bash -c "$UNMOUNT")
 else
   for dev in "$DEV" "/dev/$DEV" "/dev/disk/by-label/$DEV" "/dev/disk/by-partlabel/$DEV" ""; do
     if [[ -b $dev || -f $dev ]]; then
       sudo mount "$dev" "$TMP"
-      exec 5> >(sudo bash -c "$UNMOUNT")
+      exec 3> >(sudo bash -c "$UNMOUNT")
       break
     fi
   done
@@ -35,5 +35,5 @@ else
 fi
 
 env -C "$TMP" "$SHELL" || true
-echo >&5
+echo >&3
 wait
