0ad34454dd3fb18f0bfe7685cdaff2af9e889ad6
Pascal Diehm <pdiehm8@gmail.com>
2026-05-25 09:07:27 +0200

    feat(mnt): set idmap and symlink resolution for sshfs

--- res/bin/mnt.sh
+++ res/bin/mnt.sh
@@ -17,7 +17,7 @@ elif [[ $DEV == android ]]; then
   aft-mtp-mount "$TMP"
   exec 5> >(bash -c "$UNMOUNT")
 elif [[ $DEV == ssh://* ]]; then
-  sshfs "$(sed -E "s|^ssh://([^:]+)(:(.*))?$|\1:\3|" <<< "$DEV")" "$TMP"
+  sshfs -o idmap=user -o transform_symlinks -o follow_symlinks "$(sed -E "s|^ssh://([^:]+)(:(.*))?$|\1:\3|" <<< "$DEV")" "$TMP"
   exec 5> >(bash -c "$UNMOUNT")
 else
   for dev in "$DEV" "/dev/$DEV" "/dev/disk/by-label/$DEV" "/dev/disk/by-partlabel/$DEV" ""; do
