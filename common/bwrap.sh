fe85c8ae8a8130edcaf77320fb45086500642c3a
Pascal Diehm <pdiehm8@gmail.com>
2026-05-30 22:40:31 +0200

    feat(bwrap): drop system wide installation

--- common/bwrap.sh
+++ common/bwrap.sh
@@ -2,9 +2,8 @@
 bwrap() {
   local binary="$1" target=".local/bin/${1##*/}"
   write -ux "$target" "#!/bin/sh"
-  write -au "$target" "exec /usr/local/lib/bwrap/bwrap ${binary@Q} \"\$@\""
+  write -au "$target" "exec ~/.local/bin/bw ${binary@Q} \"\$@\""
 }
 
 package bubblewrap
-copy -x res/bin/bwrap.sh /usr/local/lib/bwrap/bwrap
 symlink -u res/bin/bwrap.sh .local/bin/bw
