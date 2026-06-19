747d7f3a926c669b35cb8ef5a3e9a747db996747
Pascal Diehm <pdiehm8@gmail.com>
2026-06-19 09:53:12 +0200

    fix: enable systemd-timesyncd

--- common/locale.sh
+++ common/locale.sh
@@ -1,9 +1,10 @@
+symlink /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+run hwclock --systohc
+systemd -e systemd-timesyncd.service
+
 copy res/locale/locale.gen /etc/locale.gen
 copy res/locale/locale.conf /etc/locale.conf
 run locale-gen
-
-symlink /usr/share/zoneinfo/Europe/Berlin /etc/localtime
-run hwclock --systohc
 
 write -a /etc/vconsole.conf "KEYMAP=de"
 run mkinitcpio --allpresets
