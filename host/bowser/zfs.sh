cbe9420aac0a7a851aa4f1c01fb8f517b9d98092
Pascal Diehm <pdiehm8@gmail.com>
2026-06-22 09:54:49 +0200

    fix(bowser): enable ZFS mkinitcpio hook

--- host/bowser/zfs.sh
+++ host/bowser/zfs.sh
@@ -3,6 +3,9 @@ run sed -i 's/Priority: high/Priority: default/' /etc/zfs/zed.d/zed-functions.sh
 systemd -e zfs.target zfs-import.target zfs-import-scan.service zfs-mount.service zfs-zed.service
 timer zfs-scrub-all weekly /usr/bin/zpool scrub -a
 
+conf /etc/mkinitcpio.conf "HOOKS=(base udev autodetect microcode block zfs filesystems root fsck)"
+run mkinitcpio --allpresets
+
 write /etc/zfs/zed.d/zed.rc << EOF
 ZED_NOTIFY_VERBOSE=1
 ZED_NTFY_URL="https://ntfy.pdiehm.dev"
