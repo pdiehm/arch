e577547be3e8106a7b90e0c5102e40ee4784855c
Pascal Diehm <pdiehm8@gmail.com>
2026-06-23 11:11:29 +0200

    fix(desktop): tmpfs mount owner

--- kind/desktop/main.sh
+++ kind/desktop/main.sh
@@ -13,7 +13,7 @@ systemd -e NetworkManager.service
 copy res/systemd/logind.conf /etc/systemd/logind.conf
 write /etc/sysctl.d/sysrq.conf "kernel.sysrq = 1"
 
-write -a /etc/fstab "tmpfs /home/pascal/Temp tmpfs defaults 0 0"
+write -a /etc/fstab "tmpfs /home/pascal/Temp tmpfs uid=1000,gid=1000 0 0"
 
 package reflector dmidecode hexedit nmap perl-image-exiftool yt-dlp vhs \
   ffmpeg mpv mpv-mpris imagemagick gimp inkscape poppler pdfpc \
