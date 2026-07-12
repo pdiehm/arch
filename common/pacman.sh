d33ed6a8a5b19e456c6626e2490474c1a14b9671
Pascal Diehm <pdiehm8@gmail.com>
2026-07-12 09:58:28 +0200

    fix(pacman): include AUR in files database

--- common/pacman.sh
+++ common/pacman.sh
@@ -1,10 +1,10 @@
 conf -e /etc/pacman.conf Color
 copy res/mirrorlist /etc/pacman.d/mirrorlist
-run pacman --files --refresh
+write -a /etc/pacman.conf "[aur]" "Server = https://pdiehm.github.io/aur"
 
 run pacman-key --add "$(use res/aur.pub)"
 run pacman-key --lsign-key "FE3A61A8A1C70F006D5718250AAB0BC4ED614894"
-write -a /etc/pacman.conf "[aur]" "Server = https://pdiehm.github.io/aur"
+run pacman --files --refresh
 
 upgrade pacman --noconfirm --sync --refresh --sysupgrade
 upgrade pacman --files --refresh
