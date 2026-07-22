8f1471f30ba9c8aa1a8133b21e376700ad559504
Pascal Diehm <pdiehm8@gmail.com>
2026-07-22 21:49:28 +0200

    feat(pascal-pc): remove system-wide libretro cores

--- host/pascal-pc/main.sh
+++ host/pascal-pc/main.sh
@@ -4,5 +4,5 @@ package tenacity kdenlive qemu-full quickemu
 package cups ipp-usb
 systemd -e cups.service ipp-usb.service
 
-package libretro
+package retroarch retroarch-assets-ozone
 persist -u .config/retroarch
