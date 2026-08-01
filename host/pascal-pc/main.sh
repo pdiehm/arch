c61035a926e916e96bfd81703e42e4a45c0b178b
Pascal Diehm <pdiehm8@gmail.com>
2026-08-01 22:56:50 +0200

    feat(via): restrict hidraw access

--- host/pascal-pc/main.sh
+++ host/pascal-pc/main.sh
@@ -1,8 +1,11 @@
 import ./network
-package tenacity kdenlive qemu-full quickemu via-bin
+package tenacity kdenlive qemu-full quickemu
 
 package cups ipp-usb
 systemd -e cups.service ipp-usb.service
+
+package via-bin
+copy res/udev/via.rules /etc/udev/rules.d/10-via.rules
 
 package retroarch retroarch-assets-ozone
 persist -u .config/retroarch
