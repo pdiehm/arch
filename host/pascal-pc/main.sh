fcb17aba9c1701066b9e5c724cde72d403b71962
Pascal Diehm <pdiehm8@gmail.com>
2026-08-11 17:41:01 +0200

    refactor(pascal-pc): reorder retroarch backups

--- host/pascal-pc/main.sh
+++ host/pascal-pc/main.sh
@@ -9,4 +9,4 @@ copy res/udev/via.rules /etc/udev/rules.d/10-via.rules
 
 package retroarch retroarch-assets-ozone
 persist -u .config/retroarch
-BACKUP+=("/home/pascal/.config/retroarch/{autoconfig,config,retroarch.cfg,saves}")
+BACKUP+=("/home/pascal/.config/retroarch/{retroarch.cfg,autoconfig,config,saves}")
