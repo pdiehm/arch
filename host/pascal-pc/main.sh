656e59736674e8b6ab1e221390e0d5bab86e0cf3
Pascal Diehm <pdiehm8@gmail.com>
2026-08-11 20:16:35 +0200

    feat(pascal-pc): add prismlauncher

--- host/pascal-pc/main.sh
+++ host/pascal-pc/main.sh
@@ -10,3 +10,7 @@ copy res/udev/via.rules /etc/udev/rules.d/10-via.rules
 package retroarch retroarch-assets-ozone
 persist -u .config/retroarch
 BACKUP+=("/home/pascal/.config/retroarch/{retroarch.cfg,autoconfig,config,saves}")
+
+package prismlauncher
+persist -u .local/share/PrismLauncher
+BACKUP+=("/home/pascal/.local/share/PrismLauncher/instances/*/minecraft/saves")
