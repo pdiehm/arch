cb1ddb69bbec5cc8e60f911c0219b83e440febce
Pascal Diehm <pdiehm8@gmail.com>
2026-08-23 22:43:07 +0200

    fix(nvim): define diff keymaps as buffer-local

--- res/nvim/ftplugin/diff.lua
+++ res/nvim/ftplugin/diff.lua
@@ -1,2 +1,2 @@
-vim.keymap.set("x", "x", ":silent! substitute/^-/ /<CR>gv:silent! global/^+/d<CR>")
-vim.keymap.set("n", "x", "Vx", { remap = true })
+vim.keymap.set("x", "x", ":silent! substitute/^-/ /<CR>gv:silent! global/^+/d<CR>", { buf = 0 })
+vim.keymap.set("n", "x", "Vx", { buf = 0, remap = true })
