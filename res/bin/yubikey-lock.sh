2247846b4924a91510520fc15bf1abd3917cffd8
Pascal Diehm <pdiehm8@gmail.com>
2026-09-04 10:03:36 +0200

    feat(yubikey): add visual feedback for lock inhibitor

--- res/bin/yubikey-lock.sh
+++ res/bin/yubikey-lock.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+shopt -s nullglob
+
 for device in /dev/input/event*; do
-  if ! evtest --query "$device" EV_KEY KEY_LEFTCTRL; then exit; fi
+  if ! evtest --query "$device" EV_KEY KEY_LEFTCTRL; then
+    for led in /sys/class/leds/*::capslock/brightness; do echo 1 > "$led"; done
+    exit
+  fi
 done
 
 loginctl lock-sessions
