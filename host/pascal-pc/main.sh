4363527f0a3c06bb3fb7863dee1aaa401c6aedc8
Pascal Diehm <pdiehm8@gmail.com>
2026-09-05 21:40:48 +0200

    feat(pascal-pc): add k3b

--- host/pascal-pc/main.sh
+++ host/pascal-pc/main.sh
@@ -15,4 +15,4 @@ persist -u .local/share/PrismLauncher
 BACKUP+=("/home/pascal/.local/share/PrismLauncher/instances/*/minecraft/saves")
 
 write /etc/systemd/system/alsa-restore.service.d/auto-mute.conf "[Service]" 'ExecStartPost=/usr/bin/amixer -c 2 sset "Auto-Mute Mode" Disabled'
-package tenacity kdenlive qemu-full quickemu
+package tenacity kdenlive k3b qemu-full quickemu
