9b61d5f129a2b48457a522300c0fa57b026cd027
Pascal Diehm <pdiehm8@gmail.com>
2026-09-19 12:28:43 +0200

    refactor(desktop): reorder module

--- kind/desktop/main.sh
+++ kind/desktop/main.sh
@@ -12,11 +12,11 @@ package tlrc
 persist -u .cache/tlrc
 timer -nu tldr-update daily /usr/bin/tldr --update
 
-write -a /etc/fstab "tmpfs /home/pascal/Temp tmpfs uid=1000,gid=1000 0 0"
-run -u mkdir Downloads
-
 copy res/systemd/logind.conf /etc/systemd/logind.conf
 write /etc/sysctl.d/sysrq.conf "kernel.sysrq = 1"
+
+write -a /etc/fstab "tmpfs /home/pascal/Temp tmpfs uid=1000,gid=1000 0 0"
+run -u mkdir Downloads
 
 package reflector hexedit nmap yt-dlp perl-image-exiftool vhs \
   mesa mesa-utils "vulkan-$HOST_GPU" vulkan-icd-loader vulkan-tools \
